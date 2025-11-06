494a561e809f07d26ed46504cb7bd15317569f8b
Reilly Dunn <155593003+ReillyDunn-UCDavis@users.noreply.github.com>
2025-11-06 15:29:40 -0800

    Add files via upload

---
+++ rock-paper-chess/scripts/drafting.gd
@@ -0,0 +1,159 @@
+class_name Drafting
+extends Node
+
+@onready var white_player : Player = get_parent().get_node("WhitePlayer")
+@onready var black_player : Player = get_parent().get_node("BlackPlayer")
+
+var turn_order : Array
+var buttons : Array
+var selections = []
+
+@onready var message : Label = $"Instruction Text"
+@onready var cam : Camera2D = get_parent().get_node("Camera2D")
+
+
+func _ready() -> void:
+	# Initialize the players
+	white_player.color = Player.Player_Color.WHITE
+	black_player.color = Player.Player_Color.BLACK
+	
+	# Pre-defined turn order
+	turn_order = [	white_player,
+					black_player, black_player,
+					white_player, white_player,
+					black_player, black_player,
+					white_player, white_player,
+					black_player, black_player,
+					white_player ]
+	
+	# Make all the drop-down menus for each piece
+	var b_pawn := make_button("Pawn", Vector2(cam.position.x - 200, cam.position.y))
+	var b_rook := make_button("Rook", Vector2(cam.position.x - 120, cam.position.y))
+	var b_knight := make_button("Knight", Vector2(cam.position.x - 40, cam.position.y))
+	var b_bishop := make_button("Bishop", Vector2(cam.position.x + 40, cam.position.y))
+	var b_queen := make_button("Queen", Vector2(cam.position.x + 120, cam.position.y))
+	var b_king := make_button("King", Vector2(cam.position.x + 200, cam.position.y))
+	buttons = [b_pawn, b_rook, b_knight, b_bishop, b_queen, b_king]
+	
+	# Put the instruction text in the scene
+	message.position.y = cam.position.y - 50
+	
+	# Run the drafting function
+	draft_controller()
+
+
+func make_button(text: String, pos: Vector2) -> MenuButton:
+	# Create button
+	var mb := MenuButton.new()
+	mb.text = text
+	
+	# Add the RPS options to drop-down menu
+	var popup := mb.get_popup()
+	popup.add_item("Rock")
+	popup.add_item("Paper")
+	popup.add_item("Scissors")
+	
+	# Position button in scene, add to scene, connect to function
+	mb.position = pos
+	add_child(mb)
+	popup.connect("id_pressed", Callable(self, "_on_item_pressed").bind(mb))
+	
+	# Customize text on button
+	mb.add_theme_color_override("font_color", Color.WHITE)
+	mb.add_theme_color_override("font_outline_color", Color.BLACK)
+	mb.add_theme_constant_override("outline_size", 2)
+	mb.set_anchors_preset(Control.PRESET_CENTER)
+	
+	return mb
+
+
+func _on_item_pressed(index: int, button: MenuButton) -> void:
+	# When an option is selected, it's put into the
+	# selections array, which the draft function is listening to.
+	var type = button.get_popup().get_item_text(index)
+	button.visible = false
+	selections.append([button, button.text, type])
+
+
+func draft_controller() -> void:
+	var current_player : Player
+	var next_player : Player
+	var player_name : String
+	
+	# X Position of the first sprite to appear (a bit hacky)
+	var sprite_xpos := -200
+	
+	for i in range(len(turn_order)):
+		# Check whose turn it is
+		current_player = turn_order[i]
+		if (current_player == white_player):
+			player_name = "White"
+		else:
+			player_name = "Black"
+		
+		# Check who will be going next
+		if (i+1 != len(turn_order)):
+			next_player = turn_order[i + 1]
+		else:
+			next_player = null
+		
+		# Make sure the player object has finished initializing
+		while not current_player.initialized:
+			await get_tree().process_frame
+		
+		# Give instructions based on if they get another turn after this
+		if (current_player == next_player):
+			message.text = player_name + ", make 2 selections."
+		else:
+			message.text = player_name + ", make 1 selection."
+		
+		# Only have the remaining options on screen.
+		# So if a player has already picked a piece, make that button disappear
+		for b in buttons:
+			if current_player.get_piece_type(b.text) != PieceTypes.Types.NONE:
+				b.visible = false
+			else:
+				b.visible = true
+		
+		# Wait for player to make a selection
+		while (selections.size() == 0):
+			await get_tree().process_frame
+		
+		# Get the selection from the array
+		var pname = selections[0][1]
+		var ptype = selections[0][2]
+		
+		# Record the choice
+		current_player.set_piece_type(pname, ptype)
+		selections.pop_front()
+		
+		# Spawn the sprite in the world
+		# These aren't the sprites the actual chess game would use,
+		# it's just so they can see what choices have been made
+		var sprite := Sprite2D.new()
+		add_child(sprite)
+		
+		# First get the correct asset
+		var sprite_filepath = "res://assets/Placeholder Assets/" + ptype + "/" + player_name + "/" + ptype + " " + pname + " " + player_name + ".png"
+		sprite.texture = load(sprite_filepath)
+	
+		# Make it a good looking size
+		sprite.scale = Vector2(0.5, 0.5)
+		
+		# Position it in the scene so they don't overlap,
+		# and are grouped by owner
+		
+		# White player in the bottom of the screen, black on top
+		var sprite_ypos : int
+		if (player_name == "White"):
+			sprite_ypos = 150
+		else:
+			sprite_ypos = -150
+		
+		sprite.position = Vector2(sprite_xpos, sprite_ypos)
+		
+		# Shift the xposition of the next sprite so they don't overlap
+		if (current_player == next_player):
+			sprite_xpos += 80
+		
+		sprite.visible = true
--- rock-paper-chess/scripts/player.gd
+++ rock-paper-chess/scripts/player.gd
@@ -3,7 +3,7 @@ extends Node
 
 enum Player_Color {WHITE, BLACK}
 
-var color : int
+var color : Player_Color
 
 var pawn : Piece
 var rook : Piece
@@ -11,6 +11,8 @@ var knight : Piece
 var bishop : Piece
 var queen : Piece
 var king : Piece
+
+var initialized : bool = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -37,3 +39,53 @@ func _ready() -> void:
 	king = Piece.new()
 	king.piece_class = PieceTypes.Classes.KING
 	king.piece_owner = color
+	
+	initialized = true
+
+
+func get_piece_type(piece_name: String) -> PieceTypes.Types:
+	match piece_name:
+		"Pawn":
+			return pawn.piece_type as PieceTypes.Types
+		"Rook":
+			return rook.piece_type as PieceTypes.Types
+		"Knight":
+			return knight.piece_type as PieceTypes.Types
+		"Bishop":
+			return bishop.piece_type as PieceTypes.Types
+		"Queen":
+			return queen.piece_type as PieceTypes.Types
+		"King":
+			return king.piece_type as PieceTypes.Types
+		_:
+			return PieceTypes.Types.NONE
+
+
+func set_piece_type(piece_name: String, type_text: String) -> void:
+	var type_enum: int
+	match type_text:
+		"Rock":
+			type_enum = PieceTypes.Types.ROCK
+		"Paper":
+			type_enum = PieceTypes.Types.PAPER
+		"Scissors":
+			type_enum = PieceTypes.Types.SCISSORS
+		_:
+			push_warning("Unknown type: %s" % type_text)
+			return
+
+	match piece_name:
+		"Pawn":
+			pawn.piece_type = type_enum
+		"Rook":
+			rook.piece_type = type_enum
+		"Knight":
+			knight.piece_type = type_enum
+		"Bishop":
+			bishop.piece_type = type_enum
+		"Queen":
+			queen.piece_type = type_enum
+		"King":
+			king.piece_type = type_enum
+		_:
+			push_warning("Unknown piece name: %s" % piece_name)
