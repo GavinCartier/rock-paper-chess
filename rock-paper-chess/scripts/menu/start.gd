b37a538fd423dd49be0cfd3b6bcd23d1405853dc
locallamp <luciazgn@gmail.com>
2025-11-22 18:37:19 -0800

    well yes!

--- rock-paper-chess/scripts/menu/start.gd
+++ rock-paper-chess/scripts/menu/start.gd
@@ -2,8 +2,8 @@ extends Button
 
 # This will need to swap to the draft screen, but for now it goes straight to the chessboard.
 @onready var chess_board = get_node("../../ChessBoard")
-@onready var menu = get_node("../../Menu")
-@onready var drafting = get_node("../../Drafting")
+@onready var menu = get_node("../..")
+@onready var drafting = get_node("../../../Drafting")
 
 func _ready():
 	var button = $"."
@@ -11,4 +11,3 @@ func _ready():
 
 func _button_pressed():
 	menu.set_visible(false)
-	drafting.set_visible(true)
