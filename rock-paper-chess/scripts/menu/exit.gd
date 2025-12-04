7716bcbe987b6cc4fb05b81af064488e504c1384
locallamp <luciazgn@gmail.com>
2025-12-03 16:19:17 -0800

    hello world

--- rock-paper-chess/scripts/menu/exit.gd
+++ rock-paper-chess/scripts/menu/exit.gd
@@ -1,10 +1,19 @@
 class_name ExitButton
 extends Button
 
+@onready var fade_transisiton = get_node("../../../FadeTransition")
+@onready var fade_animation = get_node("../../../FadeTransition/AnimationPlayer")
+@onready var fade_timer = get_node("../../../FadeTransition/FadeTimer")
+
 func _ready():
 	var button = $"."
 	button.pressed.connect(_button_pressed)
 
 func _button_pressed():
+	Sfx.play("woosh")
+	fade_transisiton.show()
+	fade_timer.start()
+	fade_animation.play("fade_in")
+	await fade_timer.timeout
 	get_tree().quit()
 	
--- rock-paper-chess/scripts/replay_button.gd
+++ rock-paper-chess/scripts/replay_button.gd
@@ -1,8 +1,18 @@
 extends Button
 
+@onready var fade_transisiton = get_node("../../FadeTransition")
+@onready var fade_animation = get_node("../../FadeTransition/AnimationPlayer")
+@onready var fade_timer = get_node("../../FadeTransition/FadeTimer")
+
 func _ready():
 	var button = $"."
 	button.pressed.connect(_replay_button_pressed)
 
 func _replay_button_pressed():
+	Sfx.play("woosh")
+	fade_transisiton.show()
+	fade_timer.start()
+	fade_animation.play("fade_in")
+	await fade_timer.timeout
+	fade_transisiton.hide()
 	get_tree().reload_current_scene()
